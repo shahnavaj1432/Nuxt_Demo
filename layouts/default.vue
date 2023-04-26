700c32d5bfec25abfb49fadb4b61e10935b68190
Shahnavaj shekh <47662128+shahnavaj1432@users.noreply.github.com>
2023-04-26 15:58:02 +0530

    First Commit
    First Commit

---
+++ layouts/default.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <NavBar />
+      <Nuxt />
+      
+    </div>
+    <!-- <Footer /> -->
+    
+  </template>
+   
+  <script>
+  import Header from '~/components/Header.vue'
+  import Footer from '~/components/Footer.vue'
+  import {NavBar} from '~/components/NavBar.vue'
+
+  export default {
+    components: {
+      Header,
+      Footer,
+      NavBar,
+     
+      
+    }
+  }
+  </script>
+
+
+<style>
+html {
+  font-family:
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+</style>
