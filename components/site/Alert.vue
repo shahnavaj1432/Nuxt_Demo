9e7789c5e72dae6769f8804b9263e17d6eb8ce1d
Shahnavaj shekh <47662128+shahnavaj1432@users.noreply.github.com>
2023-04-30 21:06:06 +0530

    alert update
    alert update

--- components/site/Alert.vue
+++ components/site/Alert.vue
@@ -1,14 +1,15 @@
 <template>
  
     <div class="flex-auto  bg-tb-300 h-8 ">
-      <span class="min-w-full">
-    <div class="text-center ">
+    
+    <div class="text-center h-8 text-decoration-line: underline; ">
       Find out where Crio grads get placed. 
       <NuxtLink to="/about" >
         Get our Placement Stats 
     </NuxtLink>
     </div>
-    </span>
+    
+   
   </div>
 
 </template>
